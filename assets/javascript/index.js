4fc221da009b6380c2c2f67029dd2b10fb39f5f0
lkarch <lkarch@protonmail.com>
2025-05-20 20:18:46 -0400

    Added primitive navbar

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -2,12 +2,50 @@ const nnRuler = "cool_tellow";
 const nnCoRuler = "ColoradoCrusade";
 const electionDate = "2025-6-30";
 
+function getCookie(cookieName) {
+  var name = cookieName + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var splitCookies = decodedCookie.split(';');
+  for(var i = 0; i < splitCookies.length; i++) {
+    let cookie = splitCookies[i];
+    while (cookie.charAt(0) == ' ') {
+      cookie = cookie.substring(1);
+    }
+    if (cookie.indexOf(name) == 0) {
+      return cookie.substring(name.length, cookie.length);
+    }
+  }
+  return "";
+}
+
 function calculateCountdown(endingDate) {
   let current = new Date();
   let end = new Date(endingDate);
   let timeDifference = end - current;
   let daysDifference = timeDifference / (1000 * 3600 * 24);
   return daysDifference;
+}
+
+function switchSection(sectionName, button) {
+  var i, section, navButton;
+  section = document.getElementsByClassName("section");
+  for (i = 0; i < section.length; i++) {
+    section[i].style.display = "none";
+  }
+
+  navButton = document.getElementsByClassName("navButton");
+  for (i = 0; i < navButton.length; i++) {
+    navButton[i].style.backgroundColor = "black";
+    navButton[i].style.color = "white";
+  }
+  
+  document.title = button.innerHTML;
+  document.cookie = "section=" + sectionName;
+
+  document.getElementById(sectionName).style.display = "block";
+
+  button.style.backgroundColor = "white";
+  button.style.color = "black";
 }
 
 function onPageLoad() {
@@ -26,5 +64,12 @@ function onPageLoad() {
     document.getElementById("electionCountdown").innerText = "The next election is RIGHT NOW!!!";
   } else {
     document.getElementById("electionCountdown").innerText = "The next election is in " + Math.ceil(calculateCountdown(electionDate)) + " days!";
+  }
+
+  console.log(document.cookie);
+  if (getCookie("section") === "") {
+    document.getElementById("electionButton").click();
+  } else {
+    document.getElementById(getCookie("section") + "Button").click();
   }
 }
