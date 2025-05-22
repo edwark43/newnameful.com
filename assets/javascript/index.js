6c10dac168a3e0ac712dd6e7a809bf1cff2e18fc
lkarch <lkarch@protonmail.com>
2025-05-22 13:14:23 -0400

    Added memberlist and url parameters

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,6 +1,11 @@
+const url = new URL(window.location.toLocaleString());
+const sectionList = document.getElementsByClassName("section");
+
 const nnRuler = "cool_tellow";
 const nnCoRuler = "ColoradoCrusade";
 const electionDate = "2025-6-30";
+
+const memberList = ["Omtegu", "pupik923", "cool_tellow", "JoeTheDauntless", "black_frieza", "Jinx64_", "eldiego", "Gero06", "Spencer1019", "mrcreeperg4", "TiredToonz", "idan1503", "Wormzy333", "ColoradoCrusade", "spenten", "Geogaddiiii", "Austcd30", "MyNameIsRasheed", "MrSteam84", "TwoGoodFiveme", "ara2009", "Ransterr", "ItzFriez6312", "ArzoAblaze", "jssjsjjssj", "toiletteeth", "B8arn", "FredDerp", "Georgequank", "hunterg2i", "KarmaIsDepressed", "Mini", "ChilliChillt", "ServerLite", "proper_cat", "Master__Kief", "ItzBiblcle", "MrAss_asd", "Burningskull562", "se2p", "mindlord", "ItIsYeFish", "pivozavr2004", "latcyy", "LuckKir", "oggghrkwneh", "MiHoub", "David", "lkarch", "MegaByteX", "ScaryCowCow4", "Glanthrial", "Deskfan45", "nielubiecie321", "BoomFox_Official", "DrDews", "DrNubXP", "hagluciak19", "SavageUser", "Fireballiceball", "JakeRedstone", "kaiookk1", "My_ChairSlipped", "j63k", "jurckurc", "workingontrying", "KevinDurantgoat", "Creeperpaste", "Islando_Commando", "Migzotic", "Teasoup", "F8fnir", "JorgenMister", "isnikoda", "TrustedHawk1855", "Revivey", "BleonIlazi", "AgentTammy", "Losangelesquest", "jeorm", "CrafterSteve98", "ilikemacandcheez", "NotRawZach", "herrydicc", "Hendway", "Pugino", "TeeDoesStuff", "TomAndBon", "SJET_Inc", "Urogalo", "arrochista", "JakePaulcraft", "WetBed43", "grammarissue", "pappoy76", "BurgerMan_27", "ilhanerdem2015", "BashurverseMC", "Batteryacids", "freddyw", "Acid0verl0ad", "CerealBoiz", "Q_QIndustries", "minerkat2011", "Capital3", "Assult_penguin", "NexSol", "DrWild", "nnnnn", "Jerry_juju", "hanxing", "Juasonxd", "Mikey_herobrine", "FluffyFoxFae", "Karmatical0", "ItsMxt", "Yen45", "Olat", "FredMCGamer", "capitanatomico", "DanielRobert15", "Hefeng_Song", "Leozero_", "MyGmail", "SkyNotBlue", "eldiegoat", "huntermkk", "ItsVollx", "sonictank1", "Miyazaki", "Beanzo0", "VolxyzMC", "tvoreal", "sushi_king86", "KAMILE33", "pascal_machet", "barton1357TF2", "Yungyoungin1", "kleeorg", "SnailRibs", "yourstruly71", "Csorroflegma", "ahhhhhhhh1587", "Lofeee", "mmakart", "adebuilder", "Jetteriter", "mud4dum", "Jesper2011", "GREENAPPLE", "guinea_pig_doody", "CreeperX200", "AudemusJura", "cutekttn", "bartur4", "Feruxia", "Kurzov", "MONKEYMAN346776", "breadcool", "Zaku1626", "keyboardan", "tetunnel", "ItTommy22Toad", "FulgencioBatista", "inoxisane", "q0iat", "Levinder", "aidenjamesmcl"];
 
 function getCookie(cookieName) {
   var name = cookieName + "=";
@@ -27,10 +32,9 @@ function calculateCountdown(endingDate) {
 }
 
 function switchSection(sectionName, button) {
-  var i, section, navButton;
-  section = document.getElementsByClassName("section");
-  for (i = 0; i < section.length; i++) {
-    section[i].style.display = "none";
+  var i, navButton;
+  for (i = 0; i < sectionList.length; i++) {
+    sectionList[i].style.display = "none";
   }
 
   navButton = document.getElementsByClassName("navButton");
@@ -48,7 +52,7 @@ function switchSection(sectionName, button) {
   button.style.color = "black";
 }
 
-function onPageLoad() {
+function loadRulersAndElectionCountdown() {
   console.log(nnRuler);
   document.getElementById("ruler").innerText = nnRuler;
   document.getElementById("rulerImage").src = "https://minotar.net/armor/body/" + nnRuler + "/100.png";
@@ -65,11 +69,29 @@ function onPageLoad() {
   } else {
     document.getElementById("electionCountdown").innerText = "The next election is in " + Math.ceil(calculateCountdown(electionDate)) + " days!";
   }
+}
 
-  console.log(document.cookie);
-  if (getCookie("section") === "") {
-    document.getElementById("electionButton").click();
+function loadMemberList() {
+  for (let member = 0; member < memberList.length; member++) {
+    const memberListItem = document.createElement("p");
+    memberListItem.innerHTML = memberList[member];
+
+    document.getElementById("memberList").appendChild(memberListItem);
+  }
+}
+
+function onPageLoad() {
+  loadRulersAndElectionCountdown()
+  loadMemberList()
+
+  if (url.searchParams.has("section")) {
+    console.log("URL parameter found, forcing section to " + url.searchParams.get("section") + ".");
+    document.getElementById(url.searchParams.get("section") + "Button").click();
+  } else if (getCookie("section") === "") {
+    console.log("A wild user appears! Defaulting to first section, \"" + sectionList[0].id + "\".")
+    document.getElementById(sectionList[0].id + "Button").click();
   } else {
+    console.log("Section cookie found, restoring section to \"" + getCookie("section") + "\".");
     document.getElementById(getCookie("section") + "Button").click();
   }
 }
